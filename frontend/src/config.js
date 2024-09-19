1b0154828965ee1bfa68b155fc3ca856e53e6156
siddhnat phase <118266052+Siddhant-0724@users.noreply.github.com>
2024-09-19 11:07:27 +0530

    Update config.js
    Bakend URl updated

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://localhost:5000/api/v1';
+export const BASE_URL = 'https://doctor-appointment-backend-8a4g.onrender.com';
 export const token = localStorage.getItem('token')
