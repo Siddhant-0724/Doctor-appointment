016d650a5f30cee4b787024449a51642bac5fafc
siddhnat phase <118266052+Siddhant-0724@users.noreply.github.com>
2024-09-19 12:42:21 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'https://doctor-appointment-backend-8a4g.onrender.com';
+export const BASE_URL = 'https://doctor-appointment-pecg.onrender.com';
 export const token = localStorage.getItem('token')
